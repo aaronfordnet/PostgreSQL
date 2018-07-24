dfdca308950f23d4b3a878939157df4a22889e5f
Aaron Ford <aaron@aaronford.net>
2018-07-24 23:27:50 +0000

    completed knex version of name lookup script

--- lookup_knex.js
+++ lookup_knex.js
@@ -6,60 +6,45 @@ const knex = require('knex')({
     host     : settings.hostname,
     user     : settings.user,
     password : settings.password,
-    database : settings.database
-    // port     : settings.port,
-    // ssl      : settings.ssl
+    database : settings.database,
+    port     : settings.port,
+    ssl      : settings.ssl
   }
 });
 
 // User input
 const userQuery = process.argv[2];
 
-
-// // Database access function
-// function dbLookup(getInput, cb) {
-  console.log('Searching...');
-  const name = getInput();
-
-  knex
-    .select('first_name', 'last_name', 'birthdate')
-    .from('famous_people')
-    .where('first_name', name)
-    //.where('first_name LIKE $1::text', [name] ))
-    //how to do binding with knex?????
-    .asCallback((err, rows) => {
-    if (err) return console.error(err);
-    console.log(rows);
-  }).finally( () => {
-  knex.destroy();
-})
-
-//
-//
-//   client.connect((err) => {
-//     if (err) {
-//       return console.error("Connection Error", err);
-//     }
-//     client.query("SELECT first_name, last_name, TO_CHAR(birthdate, 'yyyy-mm-dd') AS birthdate, ROW_NUMBER () OVER (ORDER BY first_name) FROM famous_people WHERE first_name LIKE $1::text", [name], (err, result) => {
-//       if (err) {
-//         return console.error("error running query", err);
-//       }
-//       cb(result.rows, name);
-//       client.end();
-//     });
-//   });
-// }
-
 function getInput() {
   return userQuery;
 }
 
-// // Input and output function
-// function logResult(result, query) {
-//    console.log(`Found ${result.length} person(s) with the name '${query}'`);
-//    result.forEach(person => {
-//      console.log(`${person.row_number}: ${person.first_name} ${person.last_name}, born ${person.birthdate}`);
-//    });
-// }
-//
-// dbLookup(getUserInput, logResult);
+function postOutput(result, query) {
+  let counter = 1;
+  console.log(`Found ${result.length} person(s) with the name '${query}'`);
+  result.forEach(person => {
+   console.log(`${counter}: ${person.first_name} ${person.last_name}, born ${person.birthdate}`);
+   counter++;
+  });
+}
+
+function queryDatabase(getInput, postOutput) {
+  console.log('Searching...');
+  const name = getInput();
+
+  knex('famous_people')
+    .select('first_name', 'last_name', 'birthdate')
+    .select(knex.raw("TO_CHAR(birthdate, 'yyyy-mm-dd') AS birthdate"))
+    .where('first_name', name)
+    .asCallback((err, rows) => {
+    if (err) {
+      console.error(err);
+      return false;
+    }
+    postOutput(rows, name);
+    }).finally(() => {
+      knex.destroy();
+    });
+}
+
+queryDatabase(getInput, postOutput);
